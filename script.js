4cb444c7e14495b67fb9fc375407c5e8dc8ec717
trungnt98 <nguyenthanhtrung.1381998@gmail.com>
2020-01-02 18:31:47 +0700

    add feature save tasks to localstorage

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+// --------------------------- Vùng khởi tạo biến - Phase Initializing variables ---------------------- //
 
 var newTask = document.getElementById('addNewTask');
-var btnAddTask = document.getElementById('submitTask');
-var listTask = document.getElementById('listTask');
+var btn_Add = document.getElementById('submitTask');
+var containTasks = document.getElementById('containTasks');
 var inputCheck = document.getElementsByClassName('checkTaskDone');
 var trash = document.getElementsByClassName('btnTrash');
 var header = document.getElementById('header');
+var date = document.getElementById("date");
+var listTasks = [];
 
-header.style.minHeight = header.offsetHeight + "px";
+// ---------------------------- Vùng hàm ------------------------ //
+
 // Lấy ngày tháng
 ngaythang = () => {
-
     let mangThu = ['Chủ Nhật', 'Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy']
     let today = new Date();
 
@@ -22,23 +25,49 @@ ngaythang = () => {
     return date;
 }
 
-// Hiển thị ngày tháng
+getTaskStored = () => {
 
-var date = document.getElementById("date");
-date.innerHTML = ngaythang();
+    if (localStorage.getItem('listTasks') !== null) {
+        listTasks = JSON.parse(localStorage.getItem('listTasks'));
+    }
+}
 
-
-addTask = (nametask) => {
-    let id = document.getElementsByClassName("checkTaskDone").length + 1;
-    let item = `<div class="item row">
+bindToContainer = (id, name, status) => {
+    let nameStatus = status ? 'taskDone' : '';
+    let item = `<div class="item row" id="${id}">
                     <div class="checkDone">
-                        <input type="checkbox" onchange="taskDone(this)" id="task${id}" class="checkTaskDone">
-                        <label for="task${id}"></label>
+                        <input type="checkbox" onchange="taskDone(this)" id="task_${id}" class="${nameStatus}">
+                        <label for="task_${id}" class="${nameStatus}"></label>
                     </div>
-                    <h2 class="nameTask">${nametask}</h2>
+                    <h2 class="taskName ${nameStatus} ">${name}</h2>
                     <button class="btnTrash" onclick="removeTask(this)"><i class="fas fa-trash"></i></button>
                 </div>`;
-    listTask.insertAdjacentHTML('beforeend', item);
+    containTasks.insertAdjacentHTML('beforeend', item);
+}
+
+showTask = () => {
+    containTasks.innerHTML = '';
+    listTasks.map(function (value, key) {
+        bindToContainer(key, value.name, value.done);
+    })
+}
+
+updateListTasks = () => {
+    let temp = JSON.stringify(listTasks);
+    localStorage.setItem("listTasks", temp);
+}
+
+addTask = (nametask) => {
+    let nameTask = nametask;
+    let task = {
+        name: nameTask,
+        done: false
+    }
+    listTasks.push(task);
+    let saveLocal = JSON.stringify(listTasks);
+    localStorage.setItem("listTasks", saveLocal);
+    bindToContainer(listTasks.length - 1, nameTask);
+
 }
 
 addNewTask = () => {
@@ -49,18 +78,15 @@ addNewTask = () => {
     }
 
 }
-
-toggleTaskDone = (nameTask) => {
-    nameTask.classList.toggle('taskDone');
+taskDone = (element) => {
+    let idTask = element.closest('.item').getAttribute("id");
+    listTasks[idTask]['done'] = !listTasks[idTask]['done'];
+    updateListTasks();
+    showTask();
 }
 
-taskDone = (input) => {
-    let nameTask = input.closest('.item').querySelector('.nameTask');
-    toggleTaskDone(nameTask);
-}
-
-document.addEventListener('keyup',function(e) {
-    if (e.keyCode == 13 ) {
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode == 13) {
         addNewTask();
     }
 })
@@ -71,7 +97,20 @@ labelClick = (label) => {
 }
 
 removeTask = (element) => {
-    let task = element.closest('.item');
-    listTask.removeChild(task);
-    
+    let idTask = element.closest('.item').getAttribute("id");
+    console.log(idTask);
+    listTasks.splice(idTask, 1);
+    updateListTasks();
+    showTask();
 }
+// ---------------------------- Vùng startup - Phase Start Up ------------------------ //
+
+date.innerHTML = ngaythang();
+getTaskStored();
+showTask();
+
+// localStorage.removeItem('listTasks');
+
+
+
+
